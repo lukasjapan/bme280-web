01cf7d9155e8b22aac9817cc310bd3e3ee3e92e1
Lukas <lukas@cvguy.de>
2017-09-09 12:34:21 +0900

    fix in settings

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
 const { join, resolve } = require('path')
 const { safeLoad } = require('js-yaml')
 const { readFileSync } = require('fs')
-module.exports = safeLoad(readFileSync(resolve('config', 'settings.yml')), 'utf8')['webpacker_dev_server']
+module.exports = safeLoad(readFileSync(resolve('config', 'settings-development.yml')), 'utf8')['webpacker_dev_server']
